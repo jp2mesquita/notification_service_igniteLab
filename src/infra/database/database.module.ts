08b3b00ad3c227d693059a7e2725a050ae81da2b
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-14 17:29:53 -0300

    chore: finsihing second lesson

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaNotificationsReposotory } from './prisma/repositories/prisma-notifications-repositories';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsReposotory,
+    },
+  ],
+  exports: [NotificationsRepository],
+})
+export class DatabaseModule {}
