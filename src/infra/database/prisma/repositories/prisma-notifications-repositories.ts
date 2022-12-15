fa7ddcf4ab0613451281ceaa1ddfc323a849dab0
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-15 11:01:03 -0300

    feat: implements findById and Save in prisma-notifications-repositories

--- src/infra/database/prisma/repositories/prisma-notifications-repositories.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repositories.ts
@@ -7,6 +7,9 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 @Injectable()
 export class PrismaNotificationsReposotory implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
@@ -14,5 +17,9 @@ export class PrismaNotificationsReposotory implements NotificationsRepository {
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
