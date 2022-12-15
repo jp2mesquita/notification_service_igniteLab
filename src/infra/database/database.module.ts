2ef682de45eda27d6a0eeeb877b1d2b57be728d9
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-15 09:11:54 -0300

    refactor: changing the import paths

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaNotificationsReposotory } from './prisma/repositories/prisma-notifications-repositories';
 
