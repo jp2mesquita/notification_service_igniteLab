97626dfd1c854866647ce4b49434e171cb67adaf
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-15 09:32:32 -0300

    feat: creating mappers to handle data

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -16,6 +17,10 @@ export class NotificationsController {
       content,
     });
 
-    return { notification };
+    const raw = NotificationViewModel.toHTTP(notification);
+
+    return {
+      notification: raw,
+    };
   }
 }
