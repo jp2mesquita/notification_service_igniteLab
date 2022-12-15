ddb35131216e1cf0bc0b85dd2a7f306cd067e129
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-15 11:01:36 -0300

    test: add cancel-notification test

---
+++ src/application/use-cases/cancel-notificaion.spec.ts
@@ -0,0 +1,39 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notifications';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notifications-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'Social',
+      content: new Content('Nova solicitação de amizade!'),
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].cancelAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
